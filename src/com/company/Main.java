8d96766cafd7541d4b0589bfe3a880922e3d4ef3
Julija Katukova <julija.katukova@gmail.com>
2021-06-18 20:41:06 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //These are variables I declared
+        int firstNumber;
+        int secondNumber;
+        //These are variables I initialize
+        firstNumber = 5;
+        secondNumber = 6;
+        int sum = firstNumber + secondNumber;
+//        System.out.println(sum);
+
+        float firstFloat = 0.5f;
+        float secondFloat = 2.3f;
+        float floatSum = firstFloat + secondFloat;
+//        System.out.println(floatSum);
+
+        double firstDouble = 0.7;
+        double secondDouble = 2.4;
+        double doubleSum = firstDouble + secondDouble;
+//        System.out.println(doubleSum);
+
+        String firstString = "Chips";
+        String secondString = "Chicken";
+        System.out.println("I love " + firstString + " and " + secondString);
+
+
+
+    }
+}
